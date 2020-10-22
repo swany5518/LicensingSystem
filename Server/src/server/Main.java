c0587f072c3da5b8991da65dca1ce2b8670b5786
Andrew Swanson <aswanson2018@gmail.com>
2020-10-22 05:45:00 -0500

    commented out a println

--- Server/src/server/Main.java
+++ Server/src/server/Main.java
@@ -7,7 +7,7 @@ public class Main
 		//DatabaseActions.RegisterResult rslt = DatabaseActions.attemptRegister("prism4", "password", "1aaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaab", "2aFejXKp-UAMH-cyUu-HPfc-o4uzKTo35dAo", "ipaddress");
 		//DatabaseActions.LoginResult rslt1 = DatabaseActions.attemptLogin("prism4", "password", "1aaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaab");
 		
-		System.out.println(rslt1.ordinal());
+		//System.out.println(rslt1.ordinal());
 		
 		//for (int i = 0; i < 100; ++i)
 			//DatabaseAPI.insertLicenseKey(new DatabaseAPI.LicenseKeyRow(Random.getUUID(), "12", 10, "test vendor"));
